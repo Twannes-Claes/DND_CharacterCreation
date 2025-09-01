ef9d67257118be10b362e858bc67f1ce6b6e08ec
TwannesClaes <twannes.claes@thepack.studio>
2025-09-01 15:19:21 +0200

    gamemanager to fetch data

--- Assets/Scripts/Gamemanager.cs
+++ Assets/Scripts/Gamemanager.cs
@@ -1,18 +1,40 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+using UnityEngine.Networking;
 
 public class Gamemanager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private ApiCategory categoryToFetch;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        StartCoroutine(FetchCategory(categoryToFetch));
+    }
+    private IEnumerator FetchCategory(ApiCategory category)
+    {
+        string url = ApiHelper.GetURL(category);
+
+        using (UnityWebRequest request = UnityWebRequest.Get(url))
+        {
+            request.SetRequestHeader("Accept", "application/json");
+            yield return request.SendWebRequest();
+
+            if (request.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError("Error fetching category: " + request.error);
+            }
+            else
+            {
+                string json = request.downloadHandler.text;
+
+                ApiResult result = JsonUtility.FromJson<ApiResult>(json);
+                result.category = category;
+
+                Debug.Log(json);
+            }
+        }
     }
 }
+
