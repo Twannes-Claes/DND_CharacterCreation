624362705f6895d6abffe89e5b1c0d4e01b33517
TwannesClaes <twannes.claes@thepack.studio>
2025-09-24 18:46:06 +0200

    Added code to disable all inputfields

--- Assets/Scripts/UI/CanvasSwitcher.cs
+++ Assets/Scripts/UI/CanvasSwitcher.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,6 +10,9 @@ public class CanvasSwitcher : MonoBehaviour
 
     [SerializeField]
     private Button _buttonSwitcher;
+
+    [SerializeField]
+    private List<TMPro.TMP_InputField> _inputFields = new List<TMPro.TMP_InputField>();
     #endregion
 
     #region LifeCycle
@@ -20,6 +24,17 @@ public class CanvasSwitcher : MonoBehaviour
     private void OnDisable()
     {
         _buttonSwitcher.onClick.RemoveAllListeners();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            foreach (var field in _inputFields)
+            {
+                field.interactable = !field.IsInteractable();
+            }
+        }
     }
     #endregion
 
